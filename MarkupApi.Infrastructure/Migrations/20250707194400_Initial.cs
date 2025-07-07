924aaa2cf6d7523b4b1a5023b8d20db405c4ffd0
Arpan Patel <arp.patelbiz@gmail.com>
2025-07-08 01:57:00 +0530

    Add project files.

---
+++ MarkupApi.Infrastructure/Migrations/20250707194400_Initial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarkupApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MarkupDetail",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MinMarkup = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    MaxMarkup = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    MinBaseFare = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    MaxBaseFare = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    MarkupPercentage = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    SupplierId = table.Column<int>(type: "int", nullable: true),
+                    SailingId = table.Column<int>(type: "int", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()"),
+                    UpdatedBy = table.Column<int>(type: "int", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarkupDetail", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MarkupDetail");
+        }
+    }
+}
